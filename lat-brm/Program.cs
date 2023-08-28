3b463b4db540a872076270cace3e7c3e521795f1
purylte <purylte@gmail.com>
2023-08-28 15:37:56 +0700

    Refactor jwt config

--- lat-brm/Program.cs
+++ lat-brm/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using lat_brm.Utilities;
 using lat_brm.Contracts.Utilities;
+using lat_brm.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -40,26 +41,7 @@ builder.Services.AddScoped<IRoomService, RoomService>();
 builder.Services.AddScoped<IUniversityService, UniversityService>();
 
 // JWT
-builder.Services.AddScoped<IJwtHandler, JwtHandler>();
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Issuer"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
-});
+builder.Services.AddJWTConfig(builder.Configuration);
 
 builder.Services.AddAuthorization();
 
